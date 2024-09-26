5d87778ffa35ce98e3f5d5b314885071f6caad12
DONGUK777 <kimdongyork@gmail.com>
2024-09-26 10:19:30 +0900

    some edit

--- src/hnh/main.py
+++ src/hnh/main.py
@@ -4,17 +4,12 @@ from fastapi import FastAPI, File, UploadFile
 app = FastAPI()
 
 
-@app.post("/files/")
-async def create_file(file: Annotated[bytes, File()]):
+@app.get("/files/")
+def create_file(file: Annotated[bytes, File()]):
     return {"file_size": len(file)}
 
-
-#@app.post("/uploadfile/")
-#async def create_upload_file(file: UploadFile):
-#    return {"filename": file.filename}
-
-@app.post("/predict/")
-async def pred(name: str):
+@app.get("/predict/")
+def pred(name: str):
     food = ["hot", "dog", "hotdog"]
     if name in food:
        result  = 'HotDog'
